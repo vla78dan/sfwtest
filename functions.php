925ad96a63cec3515925b6b87cf312b3a805070c
@phpsturm <vla78dan@gmail.com>
2025-01-09 11:20:17 +0300

    WP-Master.Theme development. Part1 less4_01-09

--- functions.php
+++ functions.php
@@ -2,8 +2,14 @@
 
 function sfwtest_scripts()
 {
-wp_enqueue_style('sfwtest-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+    wp_enqueue_style('sfwtest-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
 
-wp_enqueue_style('sfwtest-main', get_stylesheet_uri() . '/assets/css/main.css');
+    wp_enqueue_style('sfwtest-main', get_template_directory_uri() . '/assets/css/main.css');
+
+    wp_enqueue_script('sfwtest-bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js');
+
 }
+
+add_action('wp_enqueue_scripts', 'sfwtest_scripts');
+
 
